f0cc6810727e48f3567fe8e9fb897db94f487a89
kuma127 <kumagai27207@gmail.com>
2019-11-30 17:14:15 +0900

    スクリプト作成

---
+++ sokuho_pyocr.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import sys
+sys.path.append('/path/to/dir')
+
+import pyocr
+import pyocr.builders
+
+def print_ocr(img_path,tesseract_layout=3):
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        sys.exit(1)
+    tool = tools[0]
+
+    txt = tool.image_to_string(
+    Image.open(img_path),
+        lang='jpn',
+        builder=pyocr.builders.TextBuilder(tesseract_layout=tesseract_layout)
+    )
+    print(txt)
+
